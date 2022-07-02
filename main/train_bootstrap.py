4fd21ad22e9c513c163929abf9f0004c7b071243
kivanctezoren <58386759+kivanctezoren@users.noreply.github.com>
2022-07-03 01:30:04 +0300

    Prototype pipeline structure
    Define the datasets

--- main/train_bootstrap.py
+++ main/train_bootstrap.py
@@ -1,4 +1,38 @@
+import torchvision
+
 from lib.bootstrap import bootstrap
 from lib.ccnn import CCNN
 
-...
+
+DATASET_PATH = "datasets/"
+# Datasets that can be used
+DATASETS_LIST = ["MNIST", "FashionMNIST", "CIFAR10"]
+DATASET = "MNIST"
+
+if __name__ == "__main__":
+    if DATASET not in DATASETS_LIST:
+        raise ValueError("Given dataset is not supported: " + DATASET)
+    
+    # No transformations done by default
+    train_transform = None
+    test_transform = None
+    
+    if DATASET == "MNIST":
+        train_dset = torchvision.datasets.MNIST(DATASET_PATH, train=True, transform=train_transform)
+        test_dset = torchvision.datasets.MNIST(DATASET_PATH, train=False, transform=test_transform)
+    elif DATASET == "FashionMNIST":
+        train_dset = torchvision.datasets.FashionMNIST(DATASET_PATH, train=True, transform=train_transform)
+        test_dset = torchvision.datasets.FashionMNIST(DATASET_PATH, train=False, transform=test_transform)
+    elif DATASET == "CIFAR10":
+        train_dset = torchvision.datasets.CIFAR10(DATASET_PATH, train=True, transform=train_transform)
+        test_dset = torchvision.datasets.CIFAR10(DATASET_PATH, train=False, transform=test_transform)
+    else:
+        raise ValueError("Unrecognized dataset name: " + DATASET)
+    
+    ccnn = CCNN(...)
+    
+    # TODO: Train CCNN
+    ...
+    
+    # TODO: Apply bootstrap
+    ...
