992100562860d346cb05aa11c9bec7e59d050035
kivanctezoren <58386759+kivanctezoren@users.noreply.github.com>
2022-07-03 02:09:21 +0300

    WIP Pipeline

--- lib/ccnn.py
+++ lib/ccnn.py
@@ -1,3 +1,10 @@
-class CCNN(...):
+class CCNN:
     def __init__(self):
+        self.layer_count = 0
+        
+        self.generate_layer()  # Increments layer_count too
+        
+    def generate_layer(self):
         ...
+        
+        self.layer_count += 1
--- main/train_bootstrap.py
+++ main/train_bootstrap.py
@@ -1,3 +1,4 @@
+import torch
 import torchvision
 
 from lib.bootstrap import bootstrap
@@ -8,6 +9,8 @@ DATASET_PATH = "datasets/"
 # Datasets that can be used
 DATASETS_LIST = ["MNIST", "FashionMNIST", "CIFAR10"]
 DATASET = "MNIST"
+DATASET_TRAIN_CNT = -1
+DATASET_TEST_CNT = 1000
 
 if __name__ == "__main__":
     if DATASET not in DATASETS_LIST:
@@ -28,6 +31,15 @@ if __name__ == "__main__":
         test_dset = torchvision.datasets.CIFAR10(DATASET_PATH, train=False, transform=test_transform)
     else:
         raise ValueError("Unrecognized dataset name: " + DATASET)
+    
+    if DATASET_TRAIN_CNT > 0:
+        train_dset = torch.utils.data.Subset(train_dset, (0, DATASET_TRAIN_CNT))
+    
+    if DATASET_TEST_CNT > 0:
+        test_dset = torch.utils.data.Subset(test_dset, (0, DATASET_TEST_CNT))
+    
+    train_dl = torch.utils.data.DataLoader(...)
+    test_dl = torch.utils.data.DataLoader(...)
     
     ccnn = CCNN(...)
     
