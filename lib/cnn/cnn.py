fb7b05c83fa8392180b8dac9b7b8061948a74ae6
kivanctezoren <58386759+kivanctezoren@users.noreply.github.com>
2022-07-07 22:04:44 +0300

    Add paper LeNet parameters

--- lib/cnn/cnn.py
+++ lib/cnn/cnn.py
@@ -3,27 +3,45 @@ from torch import nn
 
 
 class LeNet(nn.Module):
-    """ The LeNet model as described in the paper by Du et al."""
+    """ A LeNet model, supporting parameters described in the paper by Du et al."""
     
-    def __init__(self, n_classes):
+    def __init__(self, n_classes, paper_params=False):
         super(LeNet, self).__init__()
         
-        self.feature_extractor = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, stride=1),
-            nn.Tanh(),
-            nn.AvgPool2d(kernel_size=2),
-            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1),
-            nn.Tanh(),
-            nn.AvgPool2d(kernel_size=2),
-            nn.Conv2d(in_channels=16, out_channels=120, kernel_size=5, stride=1),
-            nn.Tanh()
-        )
-        
-        self.classifier = nn.Sequential(
-            nn.Linear(in_features=120, out_features=84),
-            nn.Tanh(),
-            nn.Linear(in_features=84, out_features=n_classes),
-        )
+        if paper_params:  # Use parameters described in the paper
+            self.feature_extractor = nn.Sequential(
+                nn.Conv2d(in_channels=1, out_channels=32, kernel_size=2, stride=1),
+                nn.Tanh(),
+                nn.AvgPool2d(kernel_size=2),
+                nn.Conv2d(in_channels=6, out_channels=64, kernel_size=2, stride=1),
+                nn.Tanh(),
+                nn.AvgPool2d(kernel_size=2),
+                nn.Conv2d(in_channels=16, out_channels=128, kernel_size=2, stride=1),
+                nn.Tanh()
+            )
+            
+            self.classifier = nn.Sequential(
+                nn.Linear(in_features=128, out_features=84),
+                nn.Tanh(),
+                nn.Linear(in_features=84, out_features=n_classes),
+            )
+        else:  # Use parameters giving better results on MNIST dataset
+            self.feature_extractor = nn.Sequential(
+                nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, stride=1),
+                nn.Tanh(),
+                nn.AvgPool2d(kernel_size=2),
+                nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1),
+                nn.Tanh(),
+                nn.AvgPool2d(kernel_size=2),
+                nn.Conv2d(in_channels=16, out_channels=120, kernel_size=3, stride=1),
+                nn.Tanh()
+            )
+    
+            self.classifier = nn.Sequential(
+                nn.Linear(in_features=120, out_features=84),
+                nn.Tanh(),
+                nn.Linear(in_features=84, out_features=n_classes),
+            )
     
     def forward(self, x):
         x = self.feature_extractor(x)
