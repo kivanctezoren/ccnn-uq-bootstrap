89a4af646dd9308316642288a8cb90f0a1ef517d
kivanctezoren <58386759+kivanctezoren@users.noreply.github.com>
2022-07-08 00:24:27 +0300

    Fix linear layer dimensions

--- lib/cnn/cnn.py
+++ lib/cnn/cnn.py
@@ -6,8 +6,11 @@ import torch.nn.functional as F
 class LeNet(nn.Module):
     """ A LeNet model, supporting parameters described in the paper by Du et al."""
     
-    def __init__(self, n_classes, in_channels=1, paper_params=False):
+    def __init__(self, n_classes, in_channels=1, input_size=28, paper_params=False):
         super(LeNet, self).__init__()
+        
+        if input_size not in [28, 32]:
+            raise ValueError("Linear layers are not defined for input image sizes other than 28 or 32.")
         
         if paper_params:  # Use parameters described in the paper
             self.feature_extractor = nn.Sequential(
@@ -22,9 +25,9 @@ class LeNet(nn.Module):
             )
             
             self.classifier = nn.Sequential(
-                nn.Linear(in_features=128, out_features=84),
+                nn.Linear(in_features=(4608 if input_size == 32 else 3200), out_features=800),
                 nn.Tanh(),
-                nn.Linear(in_features=84, out_features=n_classes),
+                nn.Linear(in_features=800, out_features=n_classes),
             )
         else:  # Use parameters giving better results on the MNIST dataset
             self.feature_extractor = nn.Sequential(
@@ -34,19 +37,19 @@ class LeNet(nn.Module):
                 nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1),
                 nn.Tanh(),
                 nn.AvgPool2d(kernel_size=2),
-                nn.Conv2d(in_channels=16, out_channels=120, kernel_size=3, stride=1),
+                nn.Conv2d(in_channels=16, out_channels=120, kernel_size=4, stride=1),
                 nn.Tanh()
             )
     
             self.classifier = nn.Sequential(
-                nn.Linear(in_features=120, out_features=84),
+                nn.Linear(in_features=(480 if input_size == 32 else 120), out_features=84),
                 nn.Tanh(),
                 nn.Linear(in_features=84, out_features=n_classes),
             )
     
     def forward(self, x):
         x = self.feature_extractor(x)
-        x = nn.Flatten()(x)
+        x = torch.flatten(x, 1)
         logits = self.classifier(x)
         probs = F.softmax(logits, dim=1)
         return logits, probs
